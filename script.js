d93d7eca853c20d3057531ffc9cdba548565f798
GoldFNX <ryanrobert2504@gmail.com>
2023-05-26 22:41:58 -0300

    Comecei a pagina inicial colocando elementos como menu

---
+++ script.js
@@ -0,0 +1,31 @@
+const control = document.querySelectorAll('.control');
+let currentItem = 0
+const item = document.querySelectorAll('.item');
+const maxItem = item.length
+
+control.forEach(control => {
+    control.addEventListener('click', () => {
+        const isLeft = control.classList.contains('left')
+        if(isLeft) {
+            currentItem -= 2
+        } else {
+            currentItem += 2
+        }
+
+        if (currentItem >= maxItem) {
+            currentItem = 0
+        } else if (currentItem < 0) {
+            currentItem = maxItem - 1
+        }
+
+        item.forEach(item => item.classList.remove('current-item'))
+
+        item[currentItem].scrollIntoView({
+            block: "nearest",
+            inline: "start",
+            behaviour: "smooth",
+        })
+
+        item[currentItem].classList.add('current-item');
+    })
+})
